f6e0d318945ae0b08da484045dec4f58dfdb468a
Dale Jung <dale@dalejung.com>
2015-06-14 17:13:15 -0400

    removed app code

--- git_subtree/__init__.py
+++ git_subtree/__init__.py
@@ -1,9 +1,3 @@
 from subtree import SubTree
 from manager import SubTreeManager, find_treelist
 from util import find_toplevel
-
-FILE = "/Users/datacaliber/Dropbox/projects/doubleprime/domains/afab/subtrees"
-
-if __name__ == '__main__':
-
-    stm = SubTreeManager(None, FILE)
